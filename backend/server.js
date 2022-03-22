33dd2c32b9734e908d1b6fd85f66478585c69c8d
Eike J <dev.eike.jo@gmail.com>
2022-03-22 22:07:59 +0100

    added connection to db

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const colors = require("colors");
+const { connectDB } = require("./config/db");
 const dotenv = require("dotenv").config();
-const {errorhandler} = require("./middleware/errorMiddleware");
+const { errorhandler } = require("./middleware/errorMiddleware");
 
 const port = process.env.PORT || 8999;
 
-const app = express();
+connectDB();
 
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
